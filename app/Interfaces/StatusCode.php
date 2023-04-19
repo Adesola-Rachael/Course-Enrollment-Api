998dd04df811bf70735f6fb2cd00e78acb0f2223
Sola-Rachael <gtreasure162@gmail.com>
2023-04-19 02:36:24 +0100

    Task 19023 create endpoint to authenticate user

--- app/Interfaces/StatusCode.php
+++ app/Interfaces/StatusCode.php
@@ -6,7 +6,5 @@ interface StatusCode
     const OK = 200;
     const CREATED = 201;
     const VALIDATION = 422;
-    const BAD_REQUEST = 400;
     const UNAUTHORIZED = 401;
-    const SERVER_ERROR = 500;
 }
