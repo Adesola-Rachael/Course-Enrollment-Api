999e91659c6f6e71f83cd10545469a9c7d2446c8
Sola-Rachael <gtreasure162@gmail.com>
2023-05-28 17:47:16 +0100

    removed unused block from listcourseResource

--- app/Http/Resources/ListCourseResource.php
+++ app/Http/Resources/ListCourseResource.php
@@ -13,34 +13,5 @@ class ListCourseResource extends JsonResource
      * @return array
      */
     public function toArray($request)
-    {
-
-
-            // foreach ($properties as $key => $property) {
-            //     $data['items'][$key] = [
-            //         'id' => $property->id,
-            //         'street' => $property->street,
-            //         'zipcode' => $property->zipcode,
-            //         'city' => $property->city->name,
-            //         'type' => $property->type->name,
-            //         'interior' => $property->interior->type,
-            //         'bedrooms' => $property->bedrooms,
-            //         'year' => $property->year,
-            //         'surface' => $property->surface,
-            //         'available' => $property->available,
-            //         'rent' => $property->rent,
-            //         'service_costs' => $property->service_costs,
-            //         'deposit' => $property->deposit,
-            //         'text_nl' => $property->leader_text,
-            //         'office' => $property->office->name,
-            //     ];
-            //     foreach ($property->property_images as $image) {
-            //         $data['items'][$key]['images'][] = [
-            //             'id' => $image->id,
-            //         ];
-            //     }
-            // }
-            // return [
-            // ];
-        }
+    {}
 }
